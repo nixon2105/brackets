a9535778120119b521b550c22d15677e738e45a1
nixon2105 <radivilov1991@mail.ru>
2021-03-10 22:27:08 +0300

    feat: add solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+    let i = 0;
+    const brackets = bracketsConfig.length - 1;
+    while (i <= brackets) {
+        let point = bracketsConfig[i][0] + bracketsConfig[i][1];
+        i++;
+        let key = str.indexOf(point, 0);
+        while (key >= 0) {
+            str = str.slice(0, key) + str.slice(key + 2);
+            key = str.indexOf(point, 0);
+            i = 0;
+        }
+    }
+    return str.length == 0 ? true : false;
+};
